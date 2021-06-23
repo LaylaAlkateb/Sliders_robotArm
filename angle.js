9cb688f0ca50ca94f83b3478f799fb54ffa9393e
Layla | Alkateb٭ <80922570+LaylaAlkateb@users.noreply.github.com>
2021-06-23 19:41:09 +0300

    Add files via upload

---
+++ angle.js
@@ -0,0 +1,40 @@
+const Slider =document.getElementById('Slider');
+const t1 =document.getElementById('val');
+t1.innerHTML = Slider.value;
+Slider.oninput = function(){
+  t1.innerHTML = this.value;
+}
+
+const Slider2 =document.getElementById('Slider2');
+const t2 =document.getElementById('val2');
+t2.innerHTML = Slider2.value;
+Slider2.oninput = function(){
+  t2.innerHTML = this.value;
+}
+
+const Slider3 =document.getElementById('Slider3');
+const t3 =document.getElementById('val3');
+t3.innerHTML = Slider3.value;
+Slider3.oninput = function(){
+  t3.innerHTML = this.value;
+}
+const Slider4 =document.getElementById('Slider4');
+const t4 =document.getElementById('val4');
+t4.innerHTML = Slider4.value;
+Slider4.oninput = function(){
+  t4.innerHTML = this.value;
+}
+
+const Slider5 =document.getElementById('Slider5');
+const t5 =document.getElementById('val5');
+t5.innerHTML = Slider5.value;
+Slider5.oninput = function(){
+  t5.innerHTML = this.value;
+}
+
+const Slider6 =document.getElementById('Slider6');
+const t6 =document.getElementById('val6');
+t6.innerHTML = Slider6.value;
+Slider6.oninput = function(){
+  t6.innerHTML = this.value;
+}
